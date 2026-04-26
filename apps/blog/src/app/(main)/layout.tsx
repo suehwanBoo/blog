39935216aeaf4ae436e21b32bd42781e378d81cc
부수환 <rndjdieo119@naver.com>
2026-04-26 15:29:07 +0900

    fix : 디버깅 요소 삭제

--- apps/blog/src/app/(main)/layout.tsx
+++ apps/blog/src/app/(main)/layout.tsx
@@ -13,7 +13,6 @@ export default function HomeLayout({ children }: PropsWithChildren) {
       <Header>
         <Navbar />
       </Header>
-      <div style={{ height: "200vh" }}></div>
       {children}
     </>
   );
