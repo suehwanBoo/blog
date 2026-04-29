172adf966e6ffe5c32a7a2612466192e691fd479
부수환 <rndjdieo119@naver.com>
2026-04-29 12:04:49 +0900

    chore : font 최적화 및 webp 변환

--- apps/blog/src/components/layout/nav/Logo.tsx
+++ apps/blog/src/components/layout/nav/Logo.tsx
@@ -4,20 +4,14 @@ import { useTheme } from "@/context/theme-context";
 
 export default function Logo() {
   const { theme } = useTheme();
+
   const webp = theme === "light" ? "FullLogox2.webp" : "FullLogox2_dark.webp";
   const png = theme === "light" ? "FullLogox2.png" : "FullLogox2_dark.png";
 
   return (
     <picture style={{ display: "block", lineHeight: "0" }}>
       <source srcSet={webp} type="image/webp" />
-      <img
-        src={png}
-        width={140}
-        height={"auto"}
-        alt="full-logo"
-        fetchPriority="high"
-        decoding="async"
-      />
+      <img src={png} width={142} height={42} alt="full-logo" decoding="async" />
     </picture>
   );
 }
